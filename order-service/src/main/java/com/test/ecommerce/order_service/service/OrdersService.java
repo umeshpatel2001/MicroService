d67425653441aca7ea148ef66c3d169974c1d7a8
umeshpatel <umeshpatelone56@gmail.com>
2026-05-22 14:39:05 +0530

    Resilience 4J

--- order-service/src/main/java/com/test/ecommerce/order_service/service/OrdersService.java
+++ order-service/src/main/java/com/test/ecommerce/order_service/service/OrdersService.java
@@ -6,6 +6,9 @@ import com.test.ecommerce.order_service.entity.OrderItem;
 import com.test.ecommerce.order_service.entity.OrderStatus;
 import com.test.ecommerce.order_service.entity.Orders;
 import com.test.ecommerce.order_service.repository.OrdersRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -34,6 +37,9 @@ public class OrdersService {
         return modelMapper.map(orders, OrderRequestDto.class);
     }
 
+//    @Retry(name = "inventoryRetry", fallbackMethod = "createOrderFallback")
+    @CircuitBreaker(name = "inventoryCircuitBreaker", fallbackMethod = "createOrderFallback")
+    @RateLimiter(name = "inventoryRateLimiter", fallbackMethod = "createOrderFallback")
     public OrderRequestDto createOrder(OrderRequestDto orderRequestDto) {
         Double totalPrice = inventoryOpenFeignClient.reduceStocks(orderRequestDto);
         Orders orders = modelMapper.map(orderRequestDto, Orders.class);
@@ -44,5 +50,10 @@ public class OrdersService {
         orders.setOrderStatus(OrderStatus.CONFIRMED);
         Orders savedOrders = ordersRepository.save(orders);
         return modelMapper.map(savedOrders, OrderRequestDto.class);
+    }
+
+    public OrderRequestDto createOrderFallback(OrderRequestDto orderRequestDto, Throwable throwable) {
+        log.error("Falling back to create order", throwable);
+        return new OrderRequestDto();
     }
 }
